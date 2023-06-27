d888ca273ede2030b0ed2f96a1f9082dc84a77be
li0nbelenky <arie.belenky@checkmarx.com>
2023-06-27 21:36:11 +0300

    finished first draft of excercise

---
+++ src/questions.js
@@ -0,0 +1,16 @@
+export const questionsList = [{
+    question: 'Who Killed JFK?',
+    answers: [ 'Lee Harvey Oswald', 'The CIA', 'The Mafia', 'The Cubans' ],
+    correctAnswer: 0,
+  }, 
+{
+    question: 'What is the correct statement about COVID vaccines?',
+    answers: [ 'They are safe but not effective', 'They are effective but not safe', 'They are neither safe nor effective' ,'They are safe and effective'],
+    correctAnswer: 3,
+},
+{
+    question: 'who\'s the best software developer ever?',
+    answers: [ 'Linus Trovalds', 'Guido Van Rossum', 'Uncle Bob', 'You! :)' ],
+    correctAnswer: 3,
+}];
+  
